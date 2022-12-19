43d1f465d55dd5560acbd296e22257904021c16d
Joe Buhr <joe.buhr@gmail.com>
2022-12-19 09:49:28 -0600

    Updated formating

--- pc-postgres/container/files/init.sql
+++ pc-postgres/container/files/init.sql
@@ -2,7 +2,6 @@ CREATE DATABASE prisma;
 CREATE USER prisma with encrypted password 'prisma';
 GRANT ALL PRIVILEGES ON DATABASE prisma TO prisma;
 
-
 \c prisma;
 CREATE SCHEMA reporting;
 GRANT ALL PRIVILEGES ON SCHEMA reporting TO prisma;

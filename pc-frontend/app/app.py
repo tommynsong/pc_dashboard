0bec169b6d23c511e8323240704b47a9eae5e1a6
Joe Buhr <joe.buhr@gmail.com>
2022-12-19 12:42:01 -0600

    Initial Frontend.

---
+++ pc-frontend/app/app.py
@@ -0,0 +1,99 @@
+import dash
+from dash import dcc
+import dash_mantine_components as dmc
+from dash_iconify import DashIconify
+import os
+
+app = dash.Dash(__name__, use_pages=True)
+
+
+def create_nav_link(icon, label, href):
+    return dcc.Link(
+        dmc.Group(
+            [
+                dmc.ThemeIcon(
+                    DashIconify(icon=icon, width=18),
+                    size=30,
+                    radius=30,
+                    variant="light",
+                ),
+                dmc.Text(label, size="sm", color="gray"),
+            ]
+        ),
+        href=href,
+        style={"textDecoration": "none"},
+    )
+
+
+sidebar = dmc.Navbar(
+    fixed=True,
+    width={"base": 300},
+    position={"top": 80},
+    height=300,
+    children=[
+        dmc.ScrollArea(
+            offsetScrollbars=True,
+            type="scroll",
+            children=[
+                dmc.Group(
+                    direction="column",
+                    children=[
+                        create_nav_link(
+                            icon="radix-icons:rocket",
+                            label="Home",
+                            href="/",
+                        ),
+                    ],
+                ),
+                dmc.Divider(
+                    label="Defenders", style={"marginBottom": 20, "marginTop": 20}
+                ),
+                dmc.Group(
+                    direction="column",
+                    children=[
+                        create_nav_link(
+                            icon=page["icon"], label=page["name"], href=page["path"]
+                        )
+                        for page in dash.page_registry.values()
+                        if page["path"].startswith("/defenders")
+                    ],
+                ),
+                dmc.Divider(
+                    label="Vulnerabilities", style={"marginBottom": 20, "marginTop": 20}
+                ),
+                dmc.Group(
+                    direction="column",
+                    children=[
+                        create_nav_link(
+                            icon=page["icon"], label=page["name"], href=page["path"]
+                        )
+                        for page in dash.page_registry.values()
+                        if page["path"].startswith("/vulnerabilities")
+                    ],
+                ),
+            ],
+        )
+    ],
+)
+
+app.layout = dmc.Container(
+    [
+        dmc.Header(
+            height=70,
+            children=[dmc.Text("Compute Custom Reporting")],
+            style={"backgroundColor": "#228be6"},
+        ),
+        sidebar,
+        dmc.Container(
+            dash.page_container,
+            size="lg",
+            pt=20,
+            style={"marginLeft": 300},
+        ),
+    ],
+    fluid=True,
+)
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
---
+++ pc-frontend/app/pages/defenders/deployed.py
@@ -0,0 +1,39 @@
+from dash import dcc, html, Input, Output, callback, register_page
+import dash_mantine_components as dmc
+import plotly.express as px
+import pandas as pd
+import numpy
+from direct_redis import DirectRedis
+
+register_page(__name__, icon="fa:bar-chart")
+#df = pd.read_parquet('../cache/defender.parquet')
+# df2 = df.groupby(['date_added', 'category', 'version'])[
+#    'category'].count().reset_index(name='total')
+
+r = DirectRedis(host='localhost', port=6379)
+df2 = r.get('df_defenders')
+versions = df2.version.unique()
+versions = numpy.insert(versions, 0, 'all')
+
+layout = html.Div(
+    [
+        dmc.Select(
+            id="dropdown",
+            data=[{"label": x, "value": x} for x in versions],
+            value=versions[0],
+            clearable=False,
+        ),
+        dcc.Graph(id="deployed"),
+    ]
+)
+
+
+@callback(Output("deployed", "figure"), Input("dropdown", "value"))
+def update_bar_chart(version):
+    if version == 'all':
+        mask = df2["version"] != version
+    else:
+        mask = df2["version"] == version
+    fig = px.bar(df2[mask], x="date_added", y="total",
+                 color="category", barmode="stack")
+    return fig

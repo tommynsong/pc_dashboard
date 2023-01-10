0a99c93b707700fa4ab6ce10e1e58cc142d8ecd7
Joe Buhr <joe.buhr@gmail.com>
2023-01-10 16:14:49 -0600

    Added logging.  Fixed issues.

--- pc-backend/src/app.py
+++ pc-backend/src/app.py
@@ -7,9 +7,17 @@ import requests
 import psycopg2
 import psycopg2.extras
 from flask import Flask, request
+from waitress import serve
+
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG)
 
 
 def db_connect():
+    '''
+    Set up database connection
+    Return connection
+    '''
+    logging.info('Connecting to Database')
     conn = None
     try:
         conn = psycopg2.connect(
@@ -18,14 +26,23 @@ def db_connect():
             user=os.environ['POSTGRES_USER'],
             password=os.environ['POSTGRES_PASSWORD']
         )
-    except (Exception, psycopg2.DatabaseError) as msg:
-        print("Error in db_connect function:")
-        print(msg)
+    except psycopg2.OperationalError as error:
+        logging.error(error)
         return (1)
     return conn
 
 
 def init_settings():
+    '''
+    Retrieve DB conn from db_connect()
+    Configuration DB tables, if not created
+    Close DB conn
+    '''
+    connection = db_connect()
+    while connection == 1:
+        time.sleep(5)
+        connection = db_connect()
+    logging.info('Initializing Database Tables')
     create_table_sql = """
         CREATE TABLE IF NOT EXISTS reporting.settings (
             id SERIAL PRIMARY KEY,
@@ -37,14 +54,15 @@ def init_settings():
     """
     with connection:
         with connection.cursor() as cursor:
-            cursor.execute(create_table_sql)
+            try:
+                cursor.execute(create_table_sql)
+            except psycopg2.OperationalError as error:
+                logging.error(error)
+                connection.close()
+                return 1
+    connection.close()
     return 0
 
-
-connection = db_connect()
-while connection == 1:
-    time.sleep(5)
-    connection = db_connect()
 
 init_settings()
 app = Flask(__name__)
@@ -52,6 +70,13 @@ app = Flask(__name__)
 
 @app.post("/api/prismastatus")
 def prisma_status():
+    '''
+    Tests connectivity and credentials to Prisma Cloud API
+    Receives Prisma Cloud API URL, API Key, and API Secret
+    Attempts to login using supplied credentials
+    Returns 200 on success
+    '''
+    logging.info('Checking Prisma Cloud connectivity and credentials')
     data = json.loads(request.get_json())
     pc_url = (data["apiurl"] + '/login')
     payload = {
@@ -59,15 +84,32 @@ def prisma_status():
         "password": data["apisecret"],
     }
     headers = {"content-type": "application/json; charset=UTF-8"}
-    response = requests.post(pc_url, json=payload, headers=headers, timeout=10)
-    if response.status_code == 200:
-        return ({"id": 1, "message": "Successful Connection"}, 200)
-    else:
-        return ({"id": 1, "message": "Unsuccessful Connection"}, response.status_code)
+    try:
+        response = requests.post(pc_url, json=payload,
+                                 headers=headers, timeout=10)
+        if response.status_code == 200:
+            logging.info(
+                'Successfully obtained Prisma Cloud authentication token')
+            return ({"message": "Successful Connection"}, response.status_code)
+        logging.info('Unable to obtain Prisma Cloud authentication token')
+        return ({"message": "Unsuccessful Connection"}, response.status_code)
+    except requests.exceptions.RequestException as error:
+        logging.error(error)
+        return ({"message": error}, 500)
 
 
 @app.post("/api/etljobs")
 def update_etl_jobs():
+    '''
+    Creates DB connection then registers new etl jobs
+    Receives and inputs etl jobs attributes and returns 201 if created successfully
+    Closes DB connection when finished
+    '''
+    connection = db_connect()
+    while connection == 1:
+        time.sleep(5)
+        connection = db_connect()
+    logging.info('Registering etl job in DB')
     data = json.loads(request.get_json())
     conn_name = data["conn_name"]
     conn_since = data["conn_since"]
@@ -82,14 +124,25 @@ def update_etl_jobs():
     """
     with connection:
         with connection.cursor() as cursor:
-            cursor.execute(sql, (conn_name, conn_since,
-                           last_run, next_run, elapsed,
-                           retention, int_time))
-    return ({"id": 1, "message": "Connection added."}, 201)
+            try:
+                cursor.execute(sql, (conn_name, conn_since,
+                                     last_run, next_run, elapsed,
+                                     retention, int_time))
+            except psycopg2.OperationalError as error:
+                logging.error(error)
+                connection.close()
+                return ({"message": error}, 500)
+    logging.info('Successfully added etl job')
+    connection.close()
+    return ({"message": "Connection added."}, 201)
 
 
 @app.post("/api/prismasettings")
 def update_settings():
+    '''
+    Updates the Prisma Cloud settings stored in the dettings DB table
+    Receives Prisma Cloud API URL, API Key, and API Secret
+    '''
     data = json.loads(request.get_json())
     pc_url = data["apiurl"]
     pc_key = data["apikey"]
@@ -108,20 +161,53 @@ def update_settings():
         FROM reporting.settings
         WHERE type = 'prisma'
     """
+    connection = db_connect()
+    while connection == 1:
+        time.sleep(5)
+        connection = db_connect()
     with connection:
         with connection.cursor() as cursor:
-            cursor.execute(get_settings_sql)
-            row = cursor.fetchone()
-            if row:
-                cursor.execute(update_pc_settings, (pc_url, pc_key, pc_secret))
-            else:
-                cursor.execute(add_pc_settings, ('prisma',
-                                                 pc_url, pc_key, pc_secret,))
-    return ({"id": 1, "message": "Room created."}, 201)
+            logging.info('Getting Prisma Cloud settings from DB')
+            try:
+                cursor.execute(get_settings_sql)
+                row = cursor.fetchone()
+                if row:
+                    logging.info('Updating current Prisma Cloud settings')
+                    try:
+                        cursor.execute(update_pc_settings,
+                                       (pc_url, pc_key, pc_secret))
+                    except psycopg2.OperationalError as error:
+                        logging.error(error)
+                        connection.close()
+                        return ({"message": error}, 500)
+                else:
+                    logging.info('Add new Prisma Cloud settings')
+                    try:
+                        cursor.execute(add_pc_settings, ('prisma',
+                                                         pc_url, pc_key, pc_secret,))
+                    except psycopg2.OperationalError as error:
+                        logging.error(error)
+                        connection.close()
+                        return ({"message": error}, 500)
+            except psycopg2.OperationalError as error:
+                logging.error(error)
+                connection.close()
+                return ({"message": error}, 500)
+    logging.info('Successfully entered Prisma Cloud settings')
+    connection.close()
+    return ({"message": "Prisma Cloud settings successful."}, 201)
 
 
 @app.get("/api/prismasettings")
 def get_settings():
+    '''
+    Returns Prisma Cloud Settings from the DB
+    '''
+    connection = db_connect()
+    while connection == 1:
+        time.sleep(5)
+        connection = db_connect()
+    logging.info('Retrieving Prisma Cloud Settings from DB')
     with connection:
         with connection.cursor() as cursor:
             get_settings_sql = """
@@ -129,38 +215,53 @@ def get_settings():
                 FROM reporting.settings
                 WHERE type = 'prisma'
             """
-            cursor.execute(get_settings_sql)
+            try:
+                cursor.execute(get_settings_sql)
+            except psycopg2.OperationalError as error:
+                logging.error(error)
+                return ({"message": error}, 500)
             row = cursor.fetchone()
             if row:
+                logger.info('Prisma Cloud settings found and returned')
                 return ({"apiurl": row[0], "apikey": row[1], "apisecret": row[2]}, 201)
-            else:
-                return ('', 204)
+            logger.info('No Prisma Cloud settings found in DB')
+            return ('', 204)
 
 
 @app.get("/api/etljobs")
 def get_etl_jobs():
+    '''
+    Get etl jobs from DB and return
+    '''
     args = request.args
     etl_name = args.get('etl_name')
+    connection = db_connect()
+    while connection == 1:
+        time.sleep(5)
+        connection = db_connect()
+    logging.info('Getting etl job listing matching etl name from DB')
     with connection:
         with connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor) as cursor:
             if etl_name is None:
-                #sql = "SELECT jsonb_agg(to_jsonb(l)) FROM reporting.etl_jobs l"
                 sql = "SELECT * FROM reporting.etl_jobs"
             else:
                 sql = """SELECT * FROM reporting.etl_jobs
                     WHERE conn_name = \'""" + etl_name + "\'"
-                # sql = """SELECT jsonb_agg(to_jsonb(l)) FROM reporting.etl_jobs l
-                #    WHERE conn_name = \'""" + etl_name + "\'"
-            cursor.execute(sql)
-            records = cursor.fetchall()
+            try:
+                cursor.execute(sql)
+                records = cursor.fetchall()
+            except psycopg2.OperationalError as error:
+                logging.error(error)
+                connection.close()
+                return ({"message": error}, 500)
             if records:
+                logging.info('Found and returning registered etl jobs')
                 return records, 201
-            else:
-                return '', 204
+            logging.info('No registered etl jobs found')
+            return '', 204
 
 
 if __name__ == "__main__":
-    from waitress import serve
     logger = logging.getLogger('waitress')
     logger.setLevel(logging.DEBUG)
     serve(app, host="0.0.0.0", port=5050)
--- pc-defenders-deployed/src/app.py
+++ pc-defenders-deployed/src/app.py
@@ -11,14 +11,15 @@ from time import mktime
 import time
 import json
 from io import StringIO
-import sys
 import os
+import logging
 import requests
 import pandas as pd
 import psycopg2
 from direct_redis import DirectRedis
 from prismacloud.api import pc_api
 
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG)
 ETL_NAME = 'defenders_deployed'
 db_settings = {
     "host":     "postgres-edw",
@@ -30,14 +31,15 @@ db_settings = {
 
 def db_write(conn, sql):
     """Uses received db connection and executes received sql"""
+    logging.info('DB Write - %s', sql)
     cursor = conn.cursor()
     try:
         cursor.execute(sql)
-    except (Exception, psycopg2.DatabaseError) as msg:
+    except requests.exceptions.RequestException as error:
         conn.rollback()
         cursor.close()
-        print("Error in db_write function:")
-        sys.exit(msg)
+        logging.error(error)
+        return False
     conn.commit()
     cursor.close()
     return True
@@ -45,6 +47,7 @@ def db_write(conn, sql):
 
 def df_to_db(conn, df_to_write, table):
     """Writes dataframe to database table"""
+    logging.info('DF dump to table - %s', table)
     buffer = StringIO()
     df_to_write.to_csv(buffer, header=False, index=False)
     buffer.seek(0)
@@ -53,24 +56,26 @@ def df_to_db(conn, df_to_write, table):
     try:
         cursor.copy_from(buffer, table, sep=",")
         conn.commit()
-    except (Exception, psycopg2.DatabaseError) as msg:
+    except requests.exceptions.RequestException as error:
         conn.rollback()
         cursor.close()
-        print("Error in df_to_db function:")
-        sys.exit(msg)
+        logging.error(error)
+        return False
     cursor.close()
     return True
 
 
 def db_read(conn, sql):
     """Uses received db connection and executes received sql"""
+    logging.info('DB Read - %s', sql)
     q_list = []
     cursor = conn.cursor()
     try:
         cursor.execute(sql)
-    except (Exception, psycopg2.DatabaseError) as msg:
-        print("Error in db_read function:")
-        sys.exit(msg)
+    except requests.exceptions.RequestException as error:
+        cursor.close()
+        logging.error(error)
+        return False
     q_list = cursor.fetchall()
     cursor.close()
     return q_list
@@ -78,16 +83,21 @@ def db_read(conn, sql):
 
 def db_connect(params_dict):
     """Creates and returns postgres connection"""
-    conn = None
+    logging.info('Creating DB Connection')
+    l_conn = None
     try:
-        conn = psycopg2.connect(**params_dict)
-    except (Exception, psycopg2.DatabaseError) as msg:
-        print("Error in db_connect function:")
-        sys.exit(msg)
-    return conn
+        l_conn = psycopg2.connect(**params_dict)
+    except psycopg2.OperationalError as error:
+        logging.error(error)
+        return 1
+    return l_conn
 
 
 def init_defenders_table(conn):
+    '''
+    Initializing required tables for etl job
+    '''
+    logging.info('Initializing DB tables - defenders, etl_jobs')
     sql = '''
     CREATE TABLE IF NOT EXISTS reporting.defenders (
         hostname varchar (128) NOT NULL,
@@ -109,7 +119,10 @@ def init_defenders_table(conn):
     );
     GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA reporting TO prisma;
     '''
-    db_write(conn, sql)
+    if db_write(conn, sql):
+        logging.info('...success')
+    else:
+        logging.info('...failed')
 
 
 def get_run_stats():
@@ -117,14 +130,17 @@ def get_run_stats():
     Pull DB run-time stats
     '''
     url = "http://backend-api:5050/api/etljobs?etl_name=" + ETL_NAME
+    logging.info('Pulling etl job config from api endpoint - %s', url)
     try:
         response = requests.get(url, timeout=10)
-    except requests.exceptions.RequestException as msg:
-        print(msg)
-    if response.text == '':
-        return ''
-    else:
-        return (response.json())[0]
+    except requests.exceptions.RequestException as error:
+        logging.error(error)
+    if response.status_code != 201:
+        logging.info('API returned status of %s', response.status_code)
+        logging.info('No etl job results found')
+        return False
+    logging.info('ETL job instructions located')
+    return (response.json())[0]
 
 
 def add_etl_job(conn_since, next_run, last_run, elapsed, retention, int_time):
@@ -132,33 +148,75 @@ def add_etl_job(conn_since, next_run, last_run, elapsed, retention, int_time):
     Add job to the database
     '''
     url = "http://backend-api:5050/api/etljobs"
+    logging.info('Adding new etl job \'%s\' to DB', ETL_NAME)
     data = json.dumps({'conn_name': ETL_NAME, 'conn_since': conn_since,
                        'next_run': next_run, 'last_run': last_run,
                        'elapsed': elapsed, 'retention': retention,
                        'int_time': int_time}, indent=4, default=str)
-    response = requests.post(url, json=data, timeout=10)
+    try:
+        response = requests.post(url, json=data, timeout=10)
+    except requests.exceptions.RequestException as error:
+        logging.error(error)
     if response.status_code != 201:
-        return 'Error registering etl job with DB'
+        logging.error('Error registering etl job with DB')
+        return False
+    logging.info('Successfully registered etl job with DB')
+    return True
 
 
 def get_pc_creds():
+    '''
+    Get PC credentials from backend api andpoint
+    '''
+    logging.info('Getting PC creentials from backend api')
     try:
         response = requests.get(
-            'http://backend-api:5050/api/prismasettings')
+            'http://backend-api:5050/api/prismasettings', timeout=10)
         if response.status_code == 201:
             msg = ''
             if response.text != '':
+                logging.info('Credentials successfully obtained')
                 settings = response.json()
                 api_url = settings["apiurl"]
                 api_key = settings["apikey"]
                 api_secret = settings["apisecret"]
             else:
-                msg = "DB Contained no Saved Settings"
+                logging.info('DB Contained no Saved Settings')
+        elif response.status_code == 204:
+            msg = 'No Prisma Cloud credentials returned'
+            logging.info(msg)
+            return '', '', '', msg
         else:
-            msg = "Could not load settings"
-    except:
+            logging.error('Unknown response from backend api')
+    except requests.exceptions.RequestException as error:
+        logging.error(error)
         return '', '', '', 'Could not connect with Backend'
     return api_url, api_key, api_secret, msg
+
+
+def validate_pc_creds(api_url, api_key, api_secret):
+    '''
+    Returns True if validation is successful
+    '''
+    logging.info('Validating PC creentials with PC through backend api')
+    pc_url = (api_url + '/login')
+    payload = {
+        "username": api_key,
+        "password": api_secret,
+    }
+    headers = {"content-type": "application/json; charset=UTF-8"}
+    try:
+        response = requests.post(pc_url, json=payload,
+                                 headers=headers, timeout=10)
+        if response.status_code == 200:
+            logging.info(
+                'Successfully validated Prisma Cloud credentials')
+            return True
+        logging.info('Unable to validate Prisma Cloud credentials')
+        return False
+    except requests.exceptions.RequestException as error:
+        logging.error(error)
+        return False
 
 
 def main():
@@ -166,11 +224,17 @@ def main():
     Start loop with 1 minute check-in interval.
     Every checkin, look in database for next run time.
     If next run time has passed, execute next run.
+
+    Need to break this apart in the future to make more
+    supportable.
     '''
     msg = ''
     interval = 60
     while True:
         conn = db_connect(db_settings)
+        while conn == 1:
+            time.sleep(5)
+            conn = db_connect(db_settings)
         init_defenders_table(conn)
         etl_db_obj = get_run_stats()
         if etl_db_obj:
@@ -187,9 +251,8 @@ def main():
             int_time = 1
             add_etl_job(conn_since, next_run, last_run,
                         '00:00:00', retention, int_time)
-        if msg:
-            print(msg)
-        elif datetime.now() > next_run:
+        if datetime.now() > next_run:
+            logging.info('Refresh etl data initiated')
             start_time = time.time()
             date_added = [datetime.now().strftime("%Y-%m-%d")]
             (api_url, api_key, api_secret, msg) = get_pc_creds()
@@ -199,55 +262,90 @@ def main():
                     "identity": api_key,
                     "secret":   api_secret,
                 }
-                pc_api.configure(pc_settings)
+                if validate_pc_creds(api_url, api_key, api_secret):
+                    validated = True
+                else:
+                    validated = False
+                if validated:
+                    logging.info(
+                        'Configuring prismacloud.api library settings')
+                    pc_api.configure(pc_settings)
 
-                # Build dataframe from defenders api endpoint
-                defenders_api_lod = pc_api.defenders_list_read(
-                    'connected=true')
-                df_defenders = pd.DataFrame(
-                    columns=['hostname', 'version', 'type', 'category', 'connected', 'accountID', 'date_added'])
-                for defender in defenders_api_lod:
-                    if 'accountID' not in defender['cloudMetadata']:
-                        defender['cloudMetadata']['accountID'] = 'aws'
-                    df_defenders.loc[len(df_defenders.index)] = [
-                        defender['hostname'], defender['version'], defender['type'], defender['category'],
-                        defender['connected'], defender['cloudMetadata']['accountID'], date_added
-                    ]
+                    # Build dataframe from defenders api endpoint
+                    logging.info(
+                        'Pulling list of defenders from Prisma Cloud API')
+                    defenders_api_lod = pc_api.defenders_list_read(
+                        'connected=true')
+                    logging.info(
+                        'Building datafrom from defender list-of-dictionaries')
+                    df_defenders = pd.DataFrame(
+                        columns=['hostname', 'version', 'type', 'category', 'connected', 'accountID', 'date_added'])
+                    for defender in defenders_api_lod:
+                        if 'accountID' not in defender['cloudMetadata']:
+                            defender['cloudMetadata']['accountID'] = 'aws'
+                        df_defenders.loc[len(df_defenders.index)] = [
+                            defender['hostname'], defender['version'], defender['type'], defender['category'],
+                            defender['connected'], defender['cloudMetadata']['accountID'], date_added
+                        ]
 
-                # Purge old records from DB
-                sql = ("DELETE FROM reporting.defenders * WHERE date_added::date < date \'" +
-                       ((datetime.now() - timedelta(days=retention)).strftime('%Y-%m-%d')) + "\';")
-                db_write(conn, sql)
+                    # Purge old records from DB
+                    logging.info(
+                        'Purging database records older than %s days', retention)
+                    sql = ("DELETE FROM reporting.defenders * WHERE date_added::date < date \'" +
+                           ((datetime.now() - timedelta(days=retention)).strftime('%Y-%m-%d')) + "\';")
+                    db_write(conn, sql)
 
-                # Write df to defenders table
-                df_to_db(conn, df_defenders, "defenders")
+                    # Write df to defenders table
+                    logging.info('Writing defender dataframe to table')
+                    df_to_db(conn, df_defenders, "defenders")
 
-                # Pull all historical defender stats, store as dataframe
-                sql = (
-                    "SELECT category, date_added, version, connected, accountID FROM reporting.defenders")
-                data_list = db_read(conn, sql)
-                df_all_defenders = pd.DataFrame(
-                    data_list, columns=['category', 'date_added', 'version', 'connected', 'accountID'])
-                df_defenders = pd.DataFrame(
-                    data_list, columns=['category', 'date_added', 'version', 'connected', 'accountID']).groupby(
-                        ['date_added', 'category', 'version', 'connected', 'accountID'])['category'].count().reset_index(name='total')
+                    # Pull all historical defender stats, store as dataframe
+                    logging.info(
+                        'Pulling rollup data from DB for push into cache')
+                    sql = (
+                        "SELECT category, date_added, version, connected, accountID FROM reporting.defenders")
+                    data_list = db_read(conn, sql)
+                    logging.info('Converting rollup data list into dataframe')
+                    df_all_defenders = pd.DataFrame(
+                        data_list, columns=['category', 'date_added', 'version', 'connected', 'accountID'])
+                    df_defenders = pd.DataFrame(
+                        data_list, columns=['category', 'date_added', 'version', 'connected', 'accountID']).groupby(
+                            ['date_added', 'category', 'version', 'connected', 'accountID'])['category'].count().reset_index(name='total')
 
-                # Push defender dataframe to redis
-                redis_conn = DirectRedis(host='redis-cache', port=6379)
-                redis_conn.set('df_all_defenders', df_all_defenders)
-                redis_conn.set('df_defenders', df_defenders)
-
-                # Update etl_jobs with new next_run
-                next_run = datetime.now() + timedelta(int_time)
-                elapsed = time.strftime(
-                    "%H:%M:%S", time.gmtime(time.time() - start_time))
-                sql = (
-                    "UPDATE reporting.etl_jobs SET next_run = \'" +
-                    str(next_run) + "\' WHERE conn_name = \'" + ETL_NAME + "\'"
-                )
-                db_write(conn, sql)
+                    # Push defender dataframe to redis
+                    logging.info('Creating connection to redis cache')
+                    redis_conn = DirectRedis(host='redis-cache', port=6379)
+                    logging.info('Pushing rollup dataframe into cache')
+                    while True:
+                        try:
+                            redis_conn.set('df_all_defenders',
+                                           df_all_defenders)
+                            redis_conn.set('df_defenders', df_defenders)
+                            break
+                        except Exception as ex:
+                            logging.error(
+                                ex.args[0])
+                            time.sleep(5)
+                    logging.info(
+                        'Successfully stored dataframe in redis cache')
+                    # Update etl_jobs with new next_run
+                    next_run = datetime.now() + timedelta(int_time)
+                    elapsed = time.strftime(
+                        "%H:%M:%S", time.gmtime(time.time() - start_time))
+                    sql = (
+                        "UPDATE reporting.etl_jobs SET next_run = \'" +
+                        str(next_run) + "\', elapsed = \'" + str(elapsed) +
+                        "\' WHERE conn_name = \'" + ETL_NAME + "\'"
+                    )
+                    logging.info(
+                        'Updating etl job statistics with new next_run and elapsed')
+                    db_write(conn, sql)
             else:
-                print(msg)
+                logging.info(
+                    'Sleeping until credentials are available and valid')
+        else:
+            logging.info(
+                'Not time to refresh data yet.  Sleeping for %s seconds', interval)
         time.sleep(interval)
 
 
